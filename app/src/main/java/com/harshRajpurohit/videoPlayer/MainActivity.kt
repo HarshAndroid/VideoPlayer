ead851a7ae4925ef559538df8e7d8776af7d2386
AndroidHarsh <rajpurohitharsh2020@gmail.com>
2021-12-06 13:45:05 +0530

    Initial Commit.

--- app/src/main/java/com/harshRajpurohit/videoPlayer/MainActivity.kt
+++ app/src/main/java/com/harshRajpurohit/videoPlayer/MainActivity.kt
@@ -13,6 +13,7 @@ import android.view.MenuItem
 import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
@@ -26,6 +27,9 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var toggle: ActionBarDrawerToggle
+    private val sortList = arrayOf(MediaStore.Video.Media.DATE_ADDED + " DESC", MediaStore.Video.Media.DATE_ADDED,
+        MediaStore.Video.Media.TITLE, MediaStore.Video.Media.TITLE + " DESC", MediaStore.Video.Media.SIZE,
+        MediaStore.Video.Media.SIZE + " DESC")
 
     companion object{
         lateinit var videoList: ArrayList<Video>
@@ -33,6 +37,7 @@ class MainActivity : AppCompatActivity() {
         lateinit var searchList: ArrayList<Video>
         var search: Boolean = false
         var themeIndex: Int = 0
+        private var sortValue: Int = 0
         val themesList = arrayOf(R.style.coolPinkNav, R.style.coolBlueNav, R.style.coolPurpleNav, R.style.coolGreenNav,
         R.style.coolRedNav, R.style.coolBlackNav)
     }
@@ -87,7 +92,28 @@ class MainActivity : AppCompatActivity() {
                     bindingTV.themeRed.setOnClickListener { saveTheme(4) }
                     bindingTV.themeBlack.setOnClickListener { saveTheme(5) }
                 }
-                R.id.sortOrderNav-> Toast.makeText(this, "Sort Order", Toast.LENGTH_SHORT).show()
+                R.id.sortOrderNav-> {
+                    val menuItems = arrayOf("Latest", "Oldest", "Name(A to Z)", "Name(Z to A)", "File Size(Smallest)"
+                    , "File Size(Largest)")
+                    var value = sortValue
+                    val dialog = MaterialAlertDialogBuilder(this)
+                        .setTitle("Sort By")
+                        .setPositiveButton("OK"){_, _ ->
+                            val sortEditor = getSharedPreferences("Sorting", MODE_PRIVATE).edit()
+                            sortEditor.putInt("sortValue", value)
+                            sortEditor.apply()
+
+                            //for restarting app
+                            finish()
+                            startActivity(intent)
+                        }
+                        .setSingleChoiceItems(menuItems, sortValue){_, pos ->
+                            value = pos
+                        }
+                        .create()
+                    dialog.show()
+                    dialog.getButton(AlertDialog.BUTTON_POSITIVE).setBackgroundColor(Color.RED)
+                }
                 R.id.aboutNav -> startActivity(Intent(this, AboutActivity::class.java))
                 R.id.exitNav -> exitProcess(1)
             }
@@ -137,13 +163,16 @@ class MainActivity : AppCompatActivity() {
 
     @SuppressLint("InlinedApi", "Recycle", "Range")
     private fun getAllVideos(): ArrayList<Video>{
+        val sortEditor = getSharedPreferences("Sorting", MODE_PRIVATE)
+        sortValue = sortEditor.getInt("sortValue", 0)
+
         val tempList = ArrayList<Video>()
         val tempFolderList = ArrayList<String>()
         val projection = arrayOf(MediaStore.Video.Media.TITLE, MediaStore.Video.Media.SIZE, MediaStore.Video.Media._ID,
             MediaStore.Video.Media.BUCKET_DISPLAY_NAME, MediaStore.Video.Media.DATA, MediaStore.Video.Media.DATE_ADDED,
             MediaStore.Video.Media.DURATION, MediaStore.Video.Media.BUCKET_ID)
         val cursor = this.contentResolver.query(MediaStore.Video.Media.EXTERNAL_CONTENT_URI, projection, null, null,
-            MediaStore.Video.Media.DATE_ADDED + " DESC")
+            sortList[sortValue])
         if(cursor != null)
             if(cursor.moveToNext())
                 do {
