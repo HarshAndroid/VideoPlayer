470ed39a9152a27ea83da6f7a395b719feda81c3
AndroidHarsh <rajpurohitharsh2020@gmail.com>
2021-12-09 16:21:54 +0530

    Initial Commit.

--- app/src/main/java/com/harshRajpurohit/videoPlayer/VideoAdapter.kt
+++ app/src/main/java/com/harshRajpurohit/videoPlayer/VideoAdapter.kt
@@ -1,8 +1,15 @@
 package com.harshRajpurohit.videoPlayer
 
 import android.annotation.SuppressLint
+import android.app.AlertDialog
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
+import android.net.Uri
+import android.os.Build
+import android.os.Environment
+import android.provider.Settings
+import android.text.SpannableStringBuilder
 import android.text.format.DateUtils
 import android.view.LayoutInflater
 import android.view.ViewGroup
@@ -10,6 +17,10 @@ import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
+import com.google.android.material.color.MaterialColors
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.harshRajpurohit.videoPlayer.databinding.RenameFieldBinding
+import com.harshRajpurohit.videoPlayer.databinding.VideoMoreFeaturesBinding
 import com.harshRajpurohit.videoPlayer.databinding.VideoViewBinding
 
 class VideoAdapter(private val context: Context, private var videoList: ArrayList<Video>, private var isFolder: Boolean = false) : RecyclerView.Adapter<VideoAdapter.MyHolder>() {
@@ -53,6 +64,39 @@ class VideoAdapter(private val context: Context, private var videoList: ArrayLis
                 }
             }
         }
+        holder.root.setOnLongClickListener {
+
+            val customDialog = LayoutInflater.from(context).inflate(R.layout.video_more_features, holder.root, false)
+            val bindingMF = VideoMoreFeaturesBinding.bind(customDialog)
+            val dialog = MaterialAlertDialogBuilder(context).setView(customDialog)
+                .create()
+            dialog.show()
+
+            bindingMF.renameBtn.setOnClickListener {
+                requestPermissionR()
+                dialog.dismiss()
+                val customDialogRF = LayoutInflater.from(context).inflate(R.layout.rename_field, holder.root, false)
+                val bindingRF = RenameFieldBinding.bind(customDialogRF)
+                val dialogRF = MaterialAlertDialogBuilder(context).setView(customDialogRF)
+                    .setCancelable(false)
+                    .setPositiveButton("Rename"){self, _ ->
+                        self.dismiss()
+                    }
+                    .setNegativeButton("Cancel"){self, _ ->
+                        self.dismiss()
+                    }
+                    .create()
+                dialogRF.show()
+                bindingRF.renameField.text = SpannableStringBuilder(videoList[position].title)
+                dialogRF.getButton(AlertDialog.BUTTON_POSITIVE).setBackgroundColor(
+                    MaterialColors.getColor(context, R.attr.themeColor, Color.RED)
+                )
+                dialogRF.getButton(AlertDialog.BUTTON_NEGATIVE).setBackgroundColor(
+                    MaterialColors.getColor(context, R.attr.themeColor, Color.RED)
+                )
+            }
+            return@setOnLongClickListener true
+        }
     }
 
     override fun getItemCount(): Int {
@@ -69,5 +113,18 @@ class VideoAdapter(private val context: Context, private var videoList: ArrayLis
         videoList = ArrayList()
         videoList.addAll(searchList)
         notifyDataSetChanged()
+    }
+
+    //for requesting android 11 or higher storage permission
+    private fun requestPermissionR(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                if(!Environment.isExternalStorageManager()){
+                    val intent = Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION)
+                    intent.addCategory("android.intent.category.DEFAULT")
+                    intent.data = Uri.parse("package:${context.applicationContext.packageName}")
+                    ContextCompat.startActivity(context, intent, null)
+                }
+            }
+
     }
 }
